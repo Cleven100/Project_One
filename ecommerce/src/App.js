bdefe768cbb4e6db1036313f4b7a1606867a0b04
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-08-23 21:49:00 -0300

    23/08

---
+++ ecommerce/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header__logo">
+          
+      </div>
+       <div className="header__search">
+
+       </div>
+
+       
+    </div>
+  );
+}
+
+export default App;
